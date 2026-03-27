6244a10c55b52f737e6f3b5eac16d02d1b9c6e80
jpang32 <jpangallozzi@gmail.com>
2026-03-27 18:38:24 -0400

    feat(main.py): - init the main file with several features:     - Ask user for input prompt     - Call Anthropic API to parse user response into a Pydantic model containing race info     - Get response from sample race info website

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+from anthropic import Anthropic
+import requests
+from bs4 import BeautifulSoup
+
+from models.race_query import RaceQuery
+
+def main():
+    print("Hello from race-finder!")
+
+    user_query = input("What races can I help you find today?\n")
+
+    client = Anthropic()
+
+    # response = client.messages.parse(
+    #     model="claude-opus-4-6",
+    #     max_tokens=1024,
+    #     messages=[{
+    #         "role": "user",
+    #         "content": user_query
+    #     }],
+    #     output_format=RaceQuery,
+    # )
+
+    url = "https://runningintheusa.com/race/find-by-state/"
+    response = requests.get(url)
+
+    print(response.text)
+
+    # Make a search on the website using city, state, country and see how you would use each parameter
+    # USA:
+    #
+
+
+if __name__ == "__main__":
+
+    main()
